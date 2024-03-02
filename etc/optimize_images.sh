e3c41ac719aa0f693fa31022fd27eff057eea03a
Ektaynot <msinantop@gmail.com>
2024-03-02 14:21:02 +0300

    i am too tired

---
+++ etc/optimize_images.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+# Dependancies
+# - img-optimize - https://virtubox.github.io/img-optimize/
+# - imagemagick
+# - jpegoptim
+# - optipng
+
+FOLDER="/Users/ismailefetop/projects/org-blog/ismailefe_org/photos/pics"
+
+# max width
+WIDTH=1600
+
+# max height
+HEIGHT=1200
+
+#resize png or jpg to either height or width, keeps proportions using imagemagick
+find ${FOLDER} -iname '*.jpg' -o -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+img-optimize --std --path ${FOLDER}
